8f58b59d24dbbf055acdd379b74c7ad3165d19f7
Logan Shahan <loganshahan88@gmail.com>
2019-03-20 10:17:09 -0400

    added friend scores

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,3 +1,9 @@
+var path = require("path");
+
+var friends = require('../data/friends.js');
+
+module.exports = function(app){
+
 app.get("/api/friends", function(req, res) {
     return res.json(friends);
   });
@@ -8,13 +14,34 @@ app.get("/api/friends", function(req, res) {
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body parsing middleware
     var newFriend = req.body;
-  
+    var friendScore = newFriend.scores;
+
+    var match = '';
+    var matchImg = '';
+    var totalDiff = 1000;
     
   
     console.log(newFriend);
+    console.log(friendScore);
+
+    for(var i=0; i<friends.length; i++){
+      var diff = 0;
+      for(var j=0; j<friendScore; j++){
+        diff += Math.abs(friends[i].scores[j] - friendScore);
+        console.log(diff);
+      };
+      if(diff< totalDiff){
+        totalDiff = diff;
+        match = friends[i].name;
+        matchImg = friends[i].photo;
+      };
+    };
+
+
   
     friends.push(newFriend);
   
-    res.json(newFriend);
+    res.json(status: 'OK' , matchName: match, matchImage: matchImg);
   });
+};
   
--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
+var bodyParser = require("body-parser");
 
 // Sets up the Express App
 // =============================================================
@@ -9,11 +10,12 @@ var app = express();
 var PORT = process.env.PORT || 4000;
 
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.text());
 
-require("./routing/apiRoutes")(app);
-require("./routing/htmlRoutes")(app);
+require(path.join(__dirname, "./app/routing/apiRoutes"))(app);
+require(path.join(__dirname, "./app/routing/htmlRoutes"))(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
